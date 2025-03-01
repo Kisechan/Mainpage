610584351e319e07f5a3417ae8be044aca33f71c
Kisechan <Kisechan>
2025-03-01 22:22:04 +0800

    blog add

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <nav>
       <router-link to="/">首页</router-link> |
       <router-link to="/about">关于</router-link>
+      <router-link to="blog.kisechan.space/">前往博客</router-link>
     </nav>
   </header>
   <router-view />
