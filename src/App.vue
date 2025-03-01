04d22ee31b45814f12b132b4f1220b41adea1349
Kisechan <kisechan_@outlook.com>
2025-03-01 21:30:49 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <nav>
+      <router-link to="/">首页</router-link> |
+      <router-link to="/about">关于</router-link>
+    </nav>
+  </header>
+  <router-view />
+</template>
+
+<style>
+#app {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 2rem;
+  font-family: Arial, sans-serif;
+}
+nav {
+  margin-bottom: 2rem;
+}
+a {
+  color: #42b983;
+}
+</style>
