2390d5586036b6ce3ce737ce82a31486066fcaa4
Kisechan <Kisechan>
2025-03-01 22:27:52 +0800

    layout changed

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <header>
     <nav>
-      <router-link to="/">首页</router-link> |
-      <router-link to="/about">关于</router-link>
+      <router-link to="/">首页</router-link> | <router-link to="/about">关于</router-link> |
       <router-link to="blog.kisechan.space/">前往博客</router-link>
     </nav>
   </header>
